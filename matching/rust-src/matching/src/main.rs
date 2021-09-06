ab1f6b986113343998df181d10fab07bbc4d53a1
Dag Andersen <dagbjerreandersen@gmail.com>
2021-09-06 17:28:51 +0200

    cleanup

--- matching/rust-src/matching/src/main.rs
+++ matching/rust-src/matching/src/main.rs
@@ -1,11 +1,9 @@
-use std::{env, vec};
-
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::{env, vec};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}", args);
     let file = File::open(args[1].as_str())?;
     let reader = BufReader::new(file);
 
@@ -28,27 +26,25 @@ fn main() -> io::Result<()> {
         }
 
         if line.contains(":") {
-            let vec = line.split_once(':').unwrap();
-            let num = vec.0.parse::<i32>().unwrap();
-            //println!("{}", line);
-            let name: Vec<usize> = vec.1
+            let (number, pref_list) = line.split_once(':').unwrap();
+            let num = number.parse::<i32>().unwrap();
+            let prefs: Vec<usize> = pref_list
                 .split(' ')
                 .filter_map(|s| s.parse::<usize>().ok())
                 .collect();
             if num % 2 == 0 {
-                women_pref.push(name)
+                women_pref.push(prefs)
             } else {
-                men_pref.push(name)
+                men_pref.push(prefs)
             }
+            continue;
+        }
+        let (number, name) = line.split_once(' ').unwrap();
+        let num = number.parse::<i32>().unwrap();
+        if num % 2 == 0 {
+            woman_names.push(name);
         } else {
-            let vec = line.split(' ').collect::<Vec<&str>>();
-            let num = vec[0].parse::<i32>().unwrap();
-            let name = vec[1].to_string();
-            if num % 2 == 0 {
-                woman_names.push(name);
-            } else {
-                man_names.push(name);
-            }
+            man_names.push(name);
         }
     }
 
@@ -57,23 +53,16 @@ fn main() -> io::Result<()> {
     //println!("{:?}", men_pref);
     //println!("{:?}", women_pref);
 
-    let pairs = get_pairs(n, &mut men_pref, &mut women_pref);
+    let pairs = get_pairs(n, men_pref, women_pref);
 
     print_pairs(pairs, man_names, woman_names);
 
     Ok(())
 }
 
-fn get_pairs(n: usize, men_pref: &mut Vec<Vec<usize>>, women_pref: &mut Vec<Vec<usize>>) -> Vec<i32> {
-    let men_pref: Vec<Vec<usize>> = men_pref
-        .iter()
-        .map(|a| a.iter().map(|x| *x / 2 - 1).collect())
-        .collect();
-    let women_pref: Vec<Vec<usize>> = women_pref
-        .iter()
-        .map(|a| a.iter().map(|x| (*x as usize - 1) / 2).collect())
-        .map(|a| invert_woman_pref(a))
-        .collect();
+fn get_pairs(n: usize, men_pref: Vec<Vec<usize>>, women_pref: Vec<Vec<usize>>) -> Vec<i32> {
+    let men_pref = prep_men_pref(men_pref);
+    let women_pref = prep_woman_pref(women_pref);
 
     //println!("man prefs: {:?}", men_pref);
     //println!("womans prefs: {:?}", women_pref);
@@ -86,30 +75,51 @@ fn get_pairs(n: usize, men_pref: &mut Vec<Vec<usize>>, women_pref: &mut Vec<Vec<
         //println!("");
         //println!("new man = {}", free_man);
 
-        let man_pref = &men_pref[free_man as usize];
+        let man_pref = &men_pref[free_man];
 
-        while let Some(woman) = man_pref.get(men_next[free_man]) {
-            men_next[free_man as usize] = men_next[free_man] + 1;
+        while let Some(&woman) = man_pref.get(men_next[free_man]) {
+            men_next[free_man] = men_next[free_man] + 1;
 
-            let current_man = woman_partner[*woman as usize];
+            let current_man = woman_partner[woman];
             if current_man < 0 {
                 //println!("woman {} is now engaged in man {}", woman, free_man);
-                woman_partner[*woman as usize] = free_man as i32;
+                woman_partner[woman] = free_man as i32;
                 break;
-            } else if is_man_better(&women_pref[*woman as usize], current_man as usize, free_man) {
+            } else if is_man_better(&women_pref[woman], current_man as usize, free_man) {
                 free_men.push(current_man as usize);
                 //println!("woman {} prefered {} over {}", woman, free_man, current_man);
                 //println!("man {} now free", current_man);
                 //println!("woman {} is now engaged in man {}", woman, free_man);
-                woman_partner[*woman as usize] = free_man as i32;
+                woman_partner[woman] = free_man as i32;
                 break;
             } else {
-               //println!("woman {} didn't like man {}", woman, free_man);
+                //println!("woman {} didn't like man {}", woman, free_man);
             }
         }
     }
 
     woman_partner
+}
+
+fn prep_men_pref(pref: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    pref.iter()
+        .map(|a| a.iter().map(|&x| x / 2 - 1).collect())
+        .collect()
+}
+
+fn prep_woman_pref(pref: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    pref.iter()
+        .map(|a| a.iter().map(|&x| (x - 1) / 2).collect())
+        .map(|a| invert_woman_pref(a))
+        .collect()
+}
+
+fn invert_woman_pref(women_pref: Vec<usize>) -> Vec<usize> {
+    let mut wp = vec![0; women_pref.len()];
+    for (i, &m) in women_pref.iter().enumerate() {
+        wp[m] = i;
+    }
+    wp
 }
 
 fn is_man_better(women_pref: &Vec<usize>, current_man: usize, new_man: usize) -> bool {
@@ -118,17 +128,6 @@ fn is_man_better(women_pref: &Vec<usize>, current_man: usize, new_man: usize) ->
 
 fn print_pairs(woman_partner: Vec<i32>, men_names: Vec<String>, women_names: Vec<String>) {
     for (i, u) in woman_partner.into_iter().enumerate() {
-        println!(
-            "{} -- {}",
-            men_names[u as usize], women_names[i]
-        );
+        println!("{} -- {}", men_names[u as usize], women_names[i]);
     }
-}
-
-fn invert_woman_pref(women_pref: Vec<usize>) -> Vec<usize> {
-    let mut wp = vec![0; women_pref.len()];
-    for (i, m) in women_pref.iter().enumerate() {
-        wp[*m] = i;
-    }
-    wp
 }
